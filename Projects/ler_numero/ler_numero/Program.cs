71b4131842e307df40f7c9366c4bfeec491e66d4
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-26 18:10:58 -0400

    ler numero commit

---
+++ Projects/ler_numero/ler_numero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ler_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, sucessor, antecessor;
+            string texto;
+
+            Console.WriteLine("Informe um numero");
+            valor = Int32.Parse(Console.ReadLine());
+            antecessor = valor -1;
+            sucessor = valor +1;
+            texto = "numero:{0}, sucessor:{1}, antecessor: {2}";
+            Console.WriteLine(texto,valor,sucessor,antecessor);
+        }
+    }
+}
